0016e3b5dcdaaef85314adf5534ba9aa9ff0bb95
fadeyush <a.fadeyush@gmail.com>
2023-10-04 19:12:04 +0300

    Модальное окно

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useMemo, useState } from 'react';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 import MySelect from './components/UI/select/MySelect';
@@ -17,6 +18,7 @@ function App() {
   ]);
 
   const [filter, setFilter] = useState({sort:'', query:''});
+  const [modal, setModal] = useState(false)
 
   const sortedPosts = useMemo(()=>{
     if(filter.sort) {
@@ -31,7 +33,8 @@ function App() {
 
   // Принимаем newPost из дочернего компонента
   const createPost = (newPost) => {
-    setPosts([...posts, newPost])
+    setPosts([...posts, newPost]);
+    setModal(false);
   }
 
   // Принимаем post из дочернего компонента
@@ -41,7 +44,12 @@ function App() {
 
   return (
     <div className="app">
-      <PostForm create={createPost}/>
+      <MyButton style={{'marginTop': '15px'}} onClick={e=>setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin:'15px 0'}} />
       <PostFilter filter={filter} setFilter={setFilter}/>
       <PostList remove={removePost} posts={sortedAndSearchPosts} title={"Список постов JavaScript"}/>
