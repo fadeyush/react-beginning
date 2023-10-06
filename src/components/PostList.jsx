66e8dddf744a3461650d2ec98bd2f867df72e96b
fadeyush <a.fadeyush@gmail.com>
2023-10-06 15:10:02 +0300

    Анимации. React transition group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, createRef } from 'react';
+import { createRoot } from 'react-dom/client';
 import PostItem from './postItem';
+import { CSSTransition, TransitionGroup, } from 'react-transition-group';
 
 const postList = function ({posts, title, remove}) {
   if(!posts.length) {
@@ -12,9 +14,17 @@ const postList = function ({posts, title, remove}) {
   return (
       <div>
         <h1 className="posts__title">{title}</h1>
-      {posts.map((post, index)=> 
-        <PostItem remove={remove} number={index+1} post={post} key={post.id}/>
-      )}
+        <TransitionGroup>
+        {posts.map((post, index)=> 
+            <CSSTransition
+              key={post.id}
+              timeout={500}
+              classNames="post"
+            >
+            <PostItem remove={remove} number={index+1} post={post}/>
+            </CSSTransition>
+          )}
+        </TransitionGroup>
       </div>
   );
 }
