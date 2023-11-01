864d7fe8ef4ce6b775099a8e3060eb60e976f69c
fadeyush <a.fadeyush@gmail.com>
2023-11-01 14:39:38 +0300

    получение данных о конкертном посте

--- src/API/PostsService.jsx
+++ src/API/PostsService.jsx
@@ -10,4 +10,9 @@ export default class PostsService {
         });
         return response;
     }
+
+    static async getPostById(id) {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        return response;
+    }
 }
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import PostsService from '../API/PostsService';
+import { useFetching } from '../hooks/useFetching';
 const PostIdPage = () => {
+    const params = useParams();
+    const [post, setPost] = useState(null);
+    const [fetchPostById, isLoading, error] = useFetching(async(id)=>{
+        const resp = await PostsService.getPostById(id);
+        setPost(resp.data)
+      }, []);
+    
+
+    useEffect(()=> {
+        fetchPostById(params.id);
+    })
+
+    console.log(post)
+
     return (
         <h1>
-            Страница поста
+            Страница поста c ID - {params.id}
         </h1>
     );
 };
